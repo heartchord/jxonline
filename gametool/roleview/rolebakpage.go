9f68b570b19dadd34846710917eb7e5c253806e0
hc <chrislue1986@hotmail.com>
2017-01-10 04:44:20 +0800

    test1423

--- gametool/roleview/rolebakpage.go
+++ gametool/roleview/rolebakpage.go
@@ -35,7 +35,7 @@ func (pg *RoleBakPage) Create(parent *MyMainWindow) *dcl.TabPage {
 					// 目录树控件
 					dcl.TreeView{
 						AssignTo: &pg.treeView,
-						MinSize:  dcl.Size{Width: 100},
+						MinSize:  dcl.Size{Width: 100, Height: 0},
 						Font:     dcl.Font{Family: "微软雅黑", PointSize: 10},
 						Model:    pg.treeModel,
 						OnCurrentItemChanged: func() {
@@ -50,24 +50,103 @@ func (pg *RoleBakPage) Create(parent *MyMainWindow) *dcl.TabPage {
 					// 视图控件
 					dcl.WebView{
 						AssignTo:      &pg.webView,
-						MinSize:       dcl.Size{Width: 100},
+						MinSize:       dcl.Size{Width: 100, Height: 0},
 						Font:          dcl.Font{Family: "微软雅黑", PointSize: 10},
 						StretchFactor: 2,
 					},
-					dcl.VSplitter{
-						MinSize: dcl.Size{Width: 500},
+				},
+			},
+			dcl.VSplitter{
+				MinSize: dcl.Size{Width: 550, Height: 0},
+				Font:    dcl.Font{Family: "微软雅黑", PointSize: 10},
+				Children: []dcl.Widget{
+					dcl.Composite{
+						Layout: dcl.Grid{Columns: 3},
 						Children: []dcl.Widget{
-							dcl.PushButton{
-								Text: "test1",
+							dcl.Label{
+								ColumnSpan: 1,
+								Text:       "Bak文件路径:",
+							},
+							dcl.LineEdit{
+								ColumnSpan: 1,
+								Text:       "asd",
+								ReadOnly:   true,
 							},
 							dcl.PushButton{
-								Text: "test2",
+								ColumnSpan: 1,
+								Text:       "解析",
 							},
-						},
-						OnSizeChanged: func() {
-							pg.webView.Refresh()
+							dcl.TextEdit{
+								ColumnSpan: 3,
+								MinSize:    dcl.Size{Width: 100, Height: 20},
+								Text:       dcl.Bind("Remarks"),
+							},
 						},
 					},
+					dcl.Composite{
+						Layout: dcl.Grid{Columns: 6, Spacing: 10},
+						Children: []dcl.Widget{
+							dcl.Label{
+								ColumnSpan: 6,
+								Text:       "Bak头部信息:",
+							},
+							dcl.Label{
+								ColumnSpan: 1,
+								Text:       "角色名长度:",
+							},
+							dcl.LineEdit{
+								ColumnSpan: 1,
+								Text:       "asd",
+								ReadOnly:   true,
+							},
+							dcl.Label{
+								ColumnSpan: 1,
+								Text:       "角色数据长度:",
+							},
+							dcl.LineEdit{
+								ColumnSpan: 1,
+								Text:       "asd",
+								ReadOnly:   true,
+							},
+							dcl.Label{
+								ColumnSpan: 1,
+								Text:       "角色名:",
+							},
+							dcl.LineEdit{
+								ColumnSpan: 1,
+								Text:       "asd",
+								ReadOnly:   true,
+								MinSize:    dcl.Size{Width: 100, Height: 0},
+							},
+
+							dcl.Label{
+								ColumnSpan: 1,
+								Text:       "CRC1:",
+							},
+							dcl.LineEdit{
+								ColumnSpan: 1,
+								Text:       "asd",
+								ReadOnly:   true,
+							},
+							dcl.Label{
+								ColumnSpan: 1,
+								Text:       "CRC2:",
+							},
+							dcl.LineEdit{
+								ColumnSpan: 1,
+								Text:       "asd",
+								ReadOnly:   true,
+							},
+							dcl.TextEdit{
+								ColumnSpan: 6,
+								MinSize:    dcl.Size{Width: 100, Height: 20},
+							},
+						},
+					},
+				},
+
+				OnSizeChanged: func() {
+					pg.webView.Refresh()
 				},
 			},
 		},
